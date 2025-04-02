07929c986fb818640fc1c8240f6e26d6585138ea
michael-fares-lrn <michael.fares@learnosity.com>
2025-04-02 08:45:12 -0500

    [ZD27641] Host custom mathcore scorer server side for testing

---
+++ ZD27641/custom_mathcore_s.js
@@ -0,0 +1,44 @@
+LearnosityAmd.define([
+    'underscore-v1.5.2',
+    'mathcore'
+], function ( _, mathcore) {
+    'use strict';
+
+    var Scorer = function (question, response) {
+        this.question = question;
+        this.response = response;
+    };
+
+    Scorer.prototype.isValid = function () {
+        var i, temp,
+            isValid = true;
+        for (i = 0; i < this.question.specs.length; i++) {
+            // if scorer is in server side, then use mathcore.evaluateVerbose()
+            if(typeof mathcore.validate !== 'function')  {
+                temp = mathcore.evaluateVerbose(this.question.specs[i], this.response);
+            } else {
+                // if the scorer is running on the client side, then use mathcore.validate()
+                temp = mathcore.validate(this.question.specs[i], this.response);
+            }
+        
+            isValid = isValid && temp.result;
+        }
+        return isValid;
+    };
+
+    Scorer.prototype.score = function () {
+        return this.isValid() ? this.maxScore() : 0;
+    };
+
+    Scorer.prototype.maxScore = function () {
+        return this.question.score || 1;
+    };
+
+    Scorer.prototype.canValidateResponse = function () {
+        return true;
+    };
+
+    return {
+        Scorer: Scorer
+    };
+});
